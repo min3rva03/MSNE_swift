1a8f0f7aff98d7182f030b934286d16c1091aabe
Minerva <msnolasco@icloud.com>
2022-05-03 12:50:16 -0500

    Commit inicial

---
+++ TableViewExample/CountriesTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CountriesTableViewController.swift
+//  TableViewExample
+//
+//  Created by Sergio Andres Rodriguez Castillo on 25/04/22.
+//
+
+import UIKit
+
+class CountriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        //  return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return the number of rows
+        return 5
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = "Sección:  \(indexPath.section) Fila:  \(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section\(section)"
+    }
+}
